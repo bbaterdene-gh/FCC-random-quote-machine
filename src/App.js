253ec52df47ed9fe38318728967cbd75a6869975
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2021-08-03 20:54:08 +0800

    Quote functional

--- src/App.js
+++ src/App.js
@@ -1,19 +1,9 @@
+import { Quote } from "./components/Quote"
+
 export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App flex justify-center items-center w-screen h-screen bg-red-300">
+      <Quote />
     </div>
   )
 }
---
+++ src/components/Quote.jsx
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from 'react'
+import { FaQuoteLeft, FaTumblr, FaTwitter } from 'react-icons/fa'
+
+export const Quote = () => {
+  const [quote, setQuote] = useState({})
+
+  const getQuotes = async() => {
+    const url = "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json"
+    try {
+      const response = await fetch(url)
+      const json = await response.json()
+      const quotes = json.quotes
+      setQuote( quotes[ Math.floor( Math.random()*quotes.length ) ] )
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    getQuotes()
+  }, [])
+
+  return (
+    <div id="quote-box" className="w-2/5 bg-white rounded px-14 py-8">
+      <blockquote id="text" className="text-3xl text-center text-red-300">
+        <FaQuoteLeft className="inline mr-3 mb-2"/>
+        <span>{quote.quote}</span>
+      </blockquote>
+      <div id="author" className="text-right pt-3 text-red-300 font-light">- {quote.author}</div>
+      <div className="flex mt-4">
+        <a href="https://twitter.com/intent/tweet" id="tweet-quote" target="_blank" rel="noreferrer">
+          <FaTwitter className="h-full w-full bg-red-300 px-3 rounded text-white"/>
+        </a>
+        <a href="https://twitter.com/intent/tweet" id="tumblr-quote" target="_blank" rel="noreferrer"
+        className="ml-2">
+          <FaTumblr className="h-full w-full bg-red-300 px-3 rounded text-white text-xs"/>
+        </a>
+        <button id="new-quote" className="bg-red-300 px-3 py-2 rounded text-white ml-auto" onClick={() => getQuotes()}>New quote</button>
+      </div>
+    </div>
+  )
+}
