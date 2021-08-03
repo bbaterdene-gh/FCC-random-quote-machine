78eeab6a0c3c2a062b542f3d5287c521e8fc4088
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2021-08-03 22:44:51 +0800

    tailwind safelist

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,11 @@
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  purge: {
+    content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+    safelist: [
+      /^bg-/,
+      /^text-/,
+    ]
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
