b4e08d6e4fbddd1006c39a875bf9f37f8b059d8d
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2021-08-03 21:16:46 +0800

    color change

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
+import { useState } from 'react'
 import { Quote } from "./components/Quote"
 
 export const App = () => {
+  const colors = [
+    'gray',
+    'red',
+    'yellow',
+    'green',
+    'blue',
+    'indigo',
+    'purple',
+    'pink',
+  ]
+
+  const [color, setColor] = useState(colors[ Math.floor( Math.random()*colors.length ) ])
+
+  const changeColor = () => {
+    setColor( colors[ Math.floor( Math.random()*colors.length ) ] )
+  }
   return (
-    <div className="App flex justify-center items-center w-screen h-screen bg-red-300">
-      <Quote />
+    <div className={`App flex justify-center items-center w-screen h-screen bg-${color}-300`}>
+      <Quote color={color} changeColor={changeColor}/>
     </div>
   )
 }
--- src/components/Quote.jsx
+++ src/components/Quote.jsx
@@ -1,9 +1,10 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { FaQuoteLeft, FaTumblr, FaTwitter } from 'react-icons/fa'
 
-export const Quote = () => {
+export const Quote = (props) => {
   const [quote, setQuote] = useState({})
 
+  console.log(props.color)
   const getQuotes = async() => {
     const url = "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json"
     try {
@@ -22,20 +23,23 @@ export const Quote = () => {
 
   return (
     <div id="quote-box" className="w-2/5 bg-white rounded px-14 py-8">
-      <blockquote id="text" className="text-3xl text-center text-red-300">
+      <blockquote id="text" className={`text-3xl text-center text-${props.color}-400`}>
         <FaQuoteLeft className="inline mr-3 mb-2"/>
         <span>{quote.quote}</span>
       </blockquote>
-      <div id="author" className="text-right pt-3 text-red-300 font-light">- {quote.author}</div>
+      <div id="author" className={`text-right pt-3 text-${props.color}-400 font-light`}>- {quote.author}</div>
       <div className="flex mt-4">
         <a href="https://twitter.com/intent/tweet" id="tweet-quote" target="_blank" rel="noreferrer">
-          <FaTwitter className="h-full w-full bg-red-300 px-3 rounded text-white"/>
+          <FaTwitter className={`h-full w-full bg-${props.color}-400 px-3 rounded text-white`}/>
         </a>
         <a href="https://twitter.com/intent/tweet" id="tumblr-quote" target="_blank" rel="noreferrer"
         className="ml-2">
-          <FaTumblr className="h-full w-full bg-red-300 px-3 rounded text-white text-xs"/>
+          <FaTumblr className={`h-full w-full bg-${props.color}-400 px-3 rounded text-white text-xs`}/>
         </a>
-        <button id="new-quote" className="bg-red-300 px-3 py-2 rounded text-white ml-auto" onClick={() => getQuotes()}>New quote</button>
+        <button id="new-quote" className={`bg-${props.color}-400 px-3 py-2 rounded text-white ml-auto`} onClick={() => {
+          getQuotes()
+          props.changeColor()
+        }}>New quote</button>
       </div>
     </div>
   )
